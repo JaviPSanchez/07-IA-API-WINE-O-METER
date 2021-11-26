e5eeaa4401848e0fd9dd470c2861f54cce910034
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2021-11-26 21:07:53 +0100

    Add css, html, scripts, and multiple files

--- train.py
+++ train.py
@@ -1,17 +1,24 @@
+print('Importing libraries...')
 import joblib
+import os
 import pandas as pd
-import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+print('Done!')
 
-df = pd.read_csv("winequality.csv")
-#df.head()
+DATASET_PATH = "src/winequality.csv"
+print('Importing Dataset...')
+df = pd.read_csv(DATASET_PATH)
+print(df.head(5))
+print('Done...!')
 
-y = df["quality"]
+
 X = df.drop(["quality", "type"], axis=1)
+y = df["quality"]
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -23,5 +30,17 @@ model.fit(X_train, y_train)
 
 print("Accuracy: {:.2f}".format(model.score(X_test, y_test)))
 
+# Create the folder "models" if not exists
+if not os.path.exists("models"):
+    os.makedirs("models")
 
-joblib.dump(model, "model.joblib")
+
+filename = "model.joblib"
+PATH = ('D:/00_PROYECTOS_PERSONALES/DATA SCIENCE/01 - JEDHA FULL-STACK/00-Projects/05 - Bloc n°5 Industrialisation d un algorithme d apprentissage automatique et automatisation des processus de décision/ML in Production/models')
+
+
+
+if filename in os.listdir(PATH):
+    pass
+else:
+    joblib.dump(model, "models/model.joblib")
